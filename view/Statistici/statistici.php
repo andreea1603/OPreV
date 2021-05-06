5e79fad2bbd87aaf0e82080144db6939c4070b4b
andreea1603 <andreeaciocan2000@yahoo.ro>
2021-05-06 21:08:05 +0300

    fix

--- view/Statistici/statistici.php
+++ view/Statistici/statistici.php
@@ -451,11 +451,18 @@ if (mysqli_num_rows($result)) {
     
     function makeTable()
     {
-        
-        $tabel = "<table id='myChart' border='1' style='margin-top:380px;'>
-        
+
+        $tabel = "<table border='1' style='margin-top:380px;'>
+        <th></th>
         <th></th>
         <th>preobesity</th>
+        <th></th>
+        <th></th>
+        <th>overweight</th>
+        <th></th>
+        <th></th>
+        <th>obese</th>
+        <th></th>
         <tr>
         
         <th>Country</th>
@@ -463,26 +470,71 @@ if (mysqli_num_rows($result)) {
         <th>2008</th>
         <th>2014</th>
         <th>2017</th>
+        <th>2008</th>
+        <th>2014</th>
+        <th>2017</th>
+        <th>2008</th>
+        <th>2014</th>
+        <th>2017</th>
             </tr>
         ";
         $i = 0;
-        $result_countries=checkCountry();
-        $countries=$result_countries;
-        $rez = getOverweight($countries);
-        $names = $rez[0];
+        $result_countries = checkCountry();
+        $countries = $result_countries;
+
+        $rez=getPreobesity($countries);
+        $names=$rez[0];
         $pre2008 = $rez[1];
         $pre2014 = $rez[2];
         $pre2017 = $rez[3];
-        while ($i + 1 < count($names)) {
-            $tabel = $tabel."<tr>";
-            $tabel = $tabel."<td>" . $names[$i] . "</td>";
-            $tabel = $tabel."<td>" . $pre2008[$i] . "</td>";
-            $tabel = $tabel."<td>" . $pre2014[$i] . "</td>";
-            $tabel = $tabel."<td>" . $pre2017[$i] . "</td>";
-            $tabel = $tabel."</tr>";
-            $i = $i + 1; 
+
+
+        $rez2=getOverweight($countries);
+        $names1=$rez2[0];
+        $over2008 = $rez2[1];
+        $over2014 = $rez2[2];
+        $over2017 = $rez2[3];
+
+
+        $rez2=getObese($countries);
+        $names2=$rez2[0];
+        $obese2008 = $rez2[1];
+        $obese2014 = $rez2[2];
+        $obese2017 = $rez2[3];
+
+
+        while ($i + 1 < 37) {
+
+            $tabel = $tabel . "<tr>";
+
+            $tabel = $tabel . "<td>" . $names[$i] . "</td>";
+
+            $tabel = $tabel . "<td>" . $pre2008[$i] . "</td>";
+
+            $tabel = $tabel . "<td>" . $pre2014[$i] . "</td>";
+
+
+            $tabel = $tabel . "<td>" . $pre2017[$i] . "</td>";
+
+
+            $tabel = $tabel . "<td>" . $over2008[$i] . "</td>";
+
+            $tabel = $tabel . "<td>" . $over2014[$i] . "</td>";
+
+            $tabel = $tabel . "<td>" . $over2017[$i] . "</td>";
+
+
+            $tabel = $tabel . "<td>" . $obese2008[$i] . "</td>";
+
+            $tabel = $tabel . "<td>" . $obese2014[$i] . "</td>";
+
+            $tabel = $tabel . "<td>" . $obese2017[$i] . "</td>";
+
+
+            $tabel = $tabel . "</tr>";
+            $i = $i + 1;
         }
-        $tabel = $tabel."</table>";
+        $tabel = $tabel . "</table>";
         return $tabel;
     }
 
